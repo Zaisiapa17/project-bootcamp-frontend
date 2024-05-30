012ba6f2b1227ca5f4bacd60a9e722df228522b8
alie <mushyafaali@gmail.com>
2024-05-30 14:00:52 +0700

    start login page

---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+const loginPage = () => {
+    return (
+        <>
+            <section className="background-image">
+                <div>
+                    <h1>Login</h1>
+                </div>
+                <div>
+                    <h1>Logo</h1>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default loginPage
