ba0ef7ff61bffbf83772f5308739f2d2fba5adc1
alie <mushyafaali@gmail.com>
2024-06-01 15:04:12 +0700

    setup redux

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import Container from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <Container>
+            {children}
+          </Container>
         </ThemeProvider>
       </body>
     </html>
